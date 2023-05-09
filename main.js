73cee743cb91e75a348402f6927801ddc15f0dc3
Oscar León <oscarleon.alcatel@gmail.com>
2023-05-09 17:56:57 -0600

    debuger

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ function apellido() {
 
 console.log(nombre);
 apellido();
+
+//Debuger
+function restarDos(num){
+    return num-2;
+}
+
+function calcular(){
+    const sumarTotal = 4+5;
+    return restarDos(sumarTotal)
+}
+
+debugger;
+calcular();
